6145eb4ba49691bfbe461062db235a51b44462a0
anna <you@example.com>
2025-03-10 11:37:01 +0100

    go back

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,39 +1,14 @@
-#! /bin/bash
-
-#Start the service if it is not running
+#!/bin/bash
 service mariadb start
-
-#Configuration of mysql database
-
-#(enter) no root password yet
-#(n) no unix_socket authentication
-#(Y) change root password
-#new password
-#new password
-#(Y) remove anonymous users
-#(Y) disallow root login remotely, only allowed from localhost
-#(Y) remove test database
-#(Y) reload privilege tables so changes will take effect
-mysql_secure_installation << FB_EOF
-
-n
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-Y
-Y
-Y
-FB_EOF
-
-if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]
-then
-	mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE;"
-	mysql -e "CREATE USER 'MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
-	mysql -e "FLUSH PRIVILEGES;"
-	echo "User created in db successfully"
+sleep 5
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE};"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 fi
 
-service mariadb stop
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
 mysqld
